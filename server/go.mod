868bdeeaa9be911d31ff48c79c5a63d13c1db95e
kw <kacp.woja@gmail.com>
2020-06-09 03:48:05 +0200

    CORS fix

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/jcmturner/gokrb5/v8 v8.3.0 // indirect
 	github.com/lib/pq v1.6.0
+	github.com/rs/cors v1.7.0
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -5,11 +5,18 @@ import (
     "net/http"
     "log"
 
+    "github.com/rs/cors"
+
     "github.com/kacpwoja/calendar-redux/server/router"
     "github.com/kacpwoja/calendar-redux/server/eventbase"
 )
 
 func main() {
+    c := cors.New(cors.Options{
+        AllowedOrigins: []string{"*"},
+        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+    })
+
     db, err := eventbase.Init()
     if err != nil {
         log.Fatal(err)
@@ -19,5 +26,5 @@ func main() {
     fmt.Println("Starting on port 4000")
     router := router.Router()
 
-    log.Fatal(http.ListenAndServe(":4000", router))
+    log.Fatal(http.ListenAndServe(":4000", c.Handler(router)))
 }
